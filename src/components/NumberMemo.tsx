ae9c4c09473a7ccf738b7950b85aeccea129ff71
akiprogramming <akiprogramming@gmail.com>
2022-12-30 02:48:56 +0900

    feat: tmpプラス計算機能

--- src/components/NumberMemo.tsx
+++ src/components/NumberMemo.tsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
+import {
+  Center,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Text,
+} from "@chakra-ui/react";
+
 function NumberMemo() {
-  return <div>NUMEMOこれからがんばります＾＾</div>;
+  const [num1, setNum1] = useState(3);
+  const [num2, setNum2] = useState(4);
+
+  return (
+    <>
+      <p>NUMEMOこれからがんばります＾＾</p>
+      <Center>
+        <NumberInput
+          placeholder="(^^)"
+          defaultValue={num1}
+          step={1}
+          keepWithinRange={false}
+          clampValueOnBlur={false}
+          onChange={(e) => setNum1(parseInt(e))}
+          value={num1}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+        +
+        <NumberInput
+          placeholder="v(^^)v"
+          defaultValue={num2}
+          step={1}
+          keepWithinRange={false}
+          clampValueOnBlur={false}
+          onChange={(e) => setNum2(parseInt(e))}
+          value={num2}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+        =
+        <Text
+          fontSize="5xl"
+          bgClip="text"
+          bgGradient="linear(to-r, green.200, pink.500)"
+          fontWeight="extrabold"
+        >
+          {num1 + num2}
+        </Text>
+      </Center>
+    </>
+  );
 }
 
 export default NumberMemo;
