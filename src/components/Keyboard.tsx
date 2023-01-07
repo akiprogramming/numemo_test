86304e5f3341b684ee536af673de3f314a2f967f
akiprogramming <akiprogramming@gmail.com>
2023-01-08 01:47:56 +0900

    💄 キーボード配列の整頓ロジック追加

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -3,38 +3,72 @@ type Props = {
   currentValue: string;
 };
 
+const btnDisplayOrder = [
+  ...[" ", " ", "7", "8", "9", "÷"],
+  ...[" ", " ", "4", "5", "6", "×"],
+  ...[" ", " ", "1", "2", "3", "-"],
+  ...[" ", " ", "0", "C", " ", "+"],
+];
+
 export function Keyboard({ setData, currentValue }: Props) {
   const handleClickKeyboard = (value: string) => {
     setData(currentValue + value);
   };
   return (
-    <div>
-      {[
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "0",
-        "+",
-        "-",
-        "×",
-        "÷",
-      ].map((v) => {
-        return (
-          <button
-            key={v}
-            onClick={() => handleClickKeyboard(v)}
-            className="m-1 h-10 w-10 rounded-md bg-teal-300 shadow-md transition-all active:bg-teal-100 active:shadow-none"
-          >
-            {v}
-          </button>
-        );
+    <div className="mx-auto grid max-w-fit select-none grid-cols-6 grid-rows-4 items-center gap-0">
+      {btnDisplayOrder.map((btn, i) => {
+        switch (btn) {
+          case "C":
+            return (
+              <button
+                key={btn}
+                onClick={() => {
+                  setData("");
+                }}
+                className="m-1 h-10 w-10 rounded-md bg-teal-300 shadow-md transition-all active:bg-teal-100 active:shadow-none"
+              >
+                {btn}
+              </button>
+            );
+          case " ":
+          case "":
+            return (
+              <span
+                key={`empty${i}`}
+                className=" mx-auto h-10 w-10 rounded-md bg-slate-300 shadow-md transition-all active:shadow-none"
+              ></span>
+            );
+          default:
+            return (
+              <button
+                key={`btn${btn}`}
+                onClick={() => handleClickKeyboard(btn)}
+                className=" mx-auto h-10 w-10 rounded-md bg-teal-300 shadow-md transition-all active:bg-teal-100 active:shadow-none"
+              >
+                {btn}
+              </button>
+            );
+        }
       })}
+
+      {/* {[
+          {
+            label: "C",
+            pressFunc: () => {
+              setData("");
+            },
+          },
+        ].map((btn) => {
+          return (
+            <button
+              key={btn.label}
+              onClick={btn.pressFunc}
+              className="m-1 h-10 w-10 rounded-md bg-teal-300 shadow-md transition-all active:bg-teal-100 active:shadow-none"
+            >
+              {btn.label}
+            </button>
+          );
+        })} */}
     </div>
   );
 }
