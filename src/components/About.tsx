fe7cd9044838c5fb1d78f56ec03a680f97f8d3b7
akiprogramming <akiprogramming@gmail.com>
2023-01-09 23:53:14 +0900

    💄 Header幅とAboutページ内容幅調整

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,15 +2,17 @@ import { chakra } from "@chakra-ui/react";
 import { Box, Text } from "@chakra-ui/react";
 
 const ROADMAP_TASKS = [
+  { name: "キーボード幅調整", done: true },
   { name: "ローカルDB保存", done: false },
   { name: "ログイン/DB保存", done: false },
-  { name: "キーボード並び順カスタム", done: true },
+  { name: "キーボード並び順カスタム", done: false },
 ];
 
 function About() {
   return (
-    <>
+    <div className="mx-auto max-w-4xl select-none">
       <Text
+        className="text-center"
         bgGradient="linear(to-l, #7928CA, #FF0080)"
         bgClip="text"
         fontSize="6xl"
@@ -18,28 +20,30 @@ function About() {
       >
         Welcome to NUMEMO
       </Text>
-      <chakra.h1 fontSize="lg" className="font-bold">
-        Numemo(ぬめも）とは
-      </chakra.h1>
-      <Text>主にスマホでの数をメモするのに特化しています（予定）</Text>
-      <Text className="text-lg font-bold">Roadmap</Text>
-      <ul>
-        {ROADMAP_TASKS.map((task) => {
-          return (
-            <li key={task.name}>
-              <input
-                key={task.name}
-                type="checkbox"
-                className={` mr-2  ${task.done ? "line-through" : ""}`}
-                defaultChecked={task.done}
-                readOnly
-              />
-              {task.name}
-            </li>
-          );
-        })}
-      </ul>
-    </>
+      <div className="p-5">
+        <chakra.h1 fontSize="lg" className="font-bold">
+          Numemo(ぬめも）とは
+        </chakra.h1>
+        <Text>主にスマホでの数をメモするのに特化しています（予定）</Text>
+        <Text className="text-lg font-bold">Roadmap</Text>
+        <ul>
+          {ROADMAP_TASKS.map((task) => {
+            return (
+              <li key={task.name}>
+                <input
+                  key={task.name}
+                  type="checkbox"
+                  className={` mr-2  ${task.done ? "line-through" : ""}`}
+                  defaultChecked={task.done}
+                  readOnly
+                />
+                {task.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
   );
 }
 
