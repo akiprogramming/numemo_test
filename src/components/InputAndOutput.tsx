3b6cbf8e4efaa92eb4a8ee84a56877b226498c81
akiprogramming <akiprogramming@gmail.com>
2023-01-13 03:06:15 +0900

    ✨ メモ追加時にfocusとスクロール対応

--- src/components/InputAndOutput.tsx
+++ src/components/InputAndOutput.tsx
@@ -1,5 +1,11 @@
 import produce from "immer";
-import { ChangeEvent, Dispatch, FormEvent, SetStateAction } from "react";
+import {
+  ChangeEventHandler,
+  Dispatch,
+  forwardRef,
+  SetStateAction,
+  useCallback,
+} from "react";
 import { canBeNumber } from "utils/number";
 import { IoMdClose } from "react-icons/io";
 
@@ -69,60 +75,81 @@ function formatCommaNumber(num: number) {
   return num.toLocaleString("en-US");
 }
 
-export function InputAndOutput({
-  numemoInput,
-  setNumemoInputs = (e) => {},
-}: Props) {
-  const outputArray = toOutputArray(numemoInput.content);
-  const output = toOutput(outputArray);
-  const sum = calculate(outputArray);
-  const { isEditing } = numemoInput;
+export const InputAndOutput = forwardRef<HTMLInputElement, Props>(
+  ({ numemoInput, setNumemoInputs = (e) => {} }: Props, ref) => {
+    const outputArray = toOutputArray(numemoInput.content);
+    const output = toOutput(outputArray);
+    const sum = calculate(outputArray);
+    const { isEditing } = numemoInput;
 
-  return (
-    <div className={"container mx-auto flex max-w-xl flex-row "}>
-      <div className="w-full">
-        <input
-          type="text"
-          className={`mb-1 w-full rounded-full bg-blue-200 px-3 py-2 ${
-            isEditing ? " bg-orange-200" : ""
-          } `}
-          placeholder="ここに計算をメモしてください"
-          value={output}
-          onChange={(e) => {
-            setNumemoInputs(
-              produce((draft) => {
-                const typedInput = e.target.value;
-                const targetInput = draft.find((nInput) => nInput.isEditing);
-                if (targetInput) targetInput.content = typedInput;
-              })
-            );
-          }}
-        />
-        <div
-          className={`mb-3 w-full rounded-full bg-blue-400 px-3 py-2 text-gray-700 ${
-            isEditing ? " bg-orange-400" : ""
-          } `}
-        >
-          {sum !== null ? formatCommaNumber(sum) : "　"}
+    const handleClick = useCallback(() => {
+      setNumemoInputs(
+        produce((draft) => {
+          draft.forEach((nInput) => (nInput.isEditing = false));
+          const thisNumemoInput = draft.find(
+            (nInput) => nInput.id === numemoInput.id
+          );
+          if (thisNumemoInput) thisNumemoInput.isEditing = true;
+        })
+      );
+    }, []);
+
+    const handleDeleteInput = useCallback(() => {
+      setNumemoInputs(
+        produce((draft) => {
+          const isOnlyOneMemoLeft = draft.length === 1;
+          if (isOnlyOneMemoLeft) {
+            draft[0].content = "";
+            return;
+          }
+
+          const targetIndex = draft.findIndex(
+            (nInput) => nInput.id === numemoInput.id
+          );
+          draft.splice(targetIndex, 1);
+          const lastInput = draft[draft.length - 1];
+          lastInput.isEditing = true;
+        })
+      );
+    }, []);
+
+    const handleType = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+      setNumemoInputs(
+        produce((draft) => {
+          const typedInput = e.target.value;
+          const targetInput = draft.find((nInput) => nInput.isEditing);
+          if (targetInput) targetInput.content = typedInput;
+        })
+      );
+    }, []);
+
+    return (
+      <div className={"container mx-auto flex max-w-xl flex-row "}>
+        <div className="w-full" onClick={handleClick}>
+          <input
+            ref={ref}
+            type="text"
+            className={`mb-1 w-full rounded-full bg-blue-200 px-3 py-2 ${
+              isEditing ? " bg-orange-200" : ""
+            } `}
+            placeholder="ここに計算をメモしてください"
+            value={output}
+            onChange={handleType}
+          />
+          <div
+            className={`mb-3 w-full rounded-full bg-blue-400 px-3 py-2 text-gray-700 ${
+              isEditing ? " bg-orange-400" : ""
+            } `}
+          >
+            {sum !== null ? formatCommaNumber(sum) : "　"}
+          </div>
+        </div>
+        <div className="max-w-fit">
+          <button className="" onClick={handleDeleteInput}>
+            <IoMdClose />
+          </button>
         </div>
       </div>
-      <div className="max-w-fit">
-        <button
-          className=""
-          onClick={() => {
-            setNumemoInputs(
-              produce((draft) => {
-                const targetIndex = draft.findIndex(
-                  (nInput) => nInput.id === numemoInput.id
-                );
-                draft.splice(targetIndex, 1);
-              })
-            );
-          }}
-        >
-          <IoMdClose />
-        </button>
-      </div>
-    </div>
-  );
-}
+    );
+  }
+);
--- src/components/NumemoPage.tsx
+++ src/components/NumemoPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { createRef, RefObject, useEffect, useMemo, useState } from "react";
 import { InputAndOutput, NumemoInput } from "components/InputAndOutput";
 import { Keyboard } from "components/Keyboard";
 import { NUMEMO_INPUTS } from "mock/numemoData";
@@ -13,15 +13,33 @@ export function NumemoPage() {
     maxHeight: `calc(100vh - ${keyboardHeight + headerHeight}px`,
   };
 
+  const refs = useMemo(() => {
+    const refs = new Map<string, RefObject<HTMLInputElement>>();
+    numemoInputs.forEach((v) => refs.set(v.id, createRef<HTMLInputElement>()));
+    return refs;
+  }, [numemoInputs]);
+
+  useEffect(() => {
+    console.log("inputs changed side effect", numemoInputs);
+
+    const editingInput = numemoInputs.find((nInput) => nInput.isEditing);
+    if (editingInput) {
+      const editingInputRef = refs.get(editingInput.id);
+      editingInputRef?.current?.focus();
+      editingInputRef?.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [numemoInputs]);
+
   return (
     <>
       <div style={numemoListMaxHeightStyle} className="overflow-y-auto py-1">
-        {numemoInputs.map((inputItem) => {
+        {numemoInputs.map((inputItem, i) => {
           return (
             <InputAndOutput
               numemoInput={inputItem}
               setNumemoInputs={setNumemoInputs}
               key={inputItem.id}
+              ref={refs.get(inputItem.id)}
             />
           );
         })}
