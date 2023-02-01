b0490811b875881f35d573d24475d3a09fbed789
akiprogramming <akiprogramming@gmail.com>
2023-02-02 00:43:50 +0900

    ✨ PWA設定調整

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), VitePWA(pwaOptions)],
   // plugins: [
   //   VitePWA({
   //     registerType: "autoUpdate",
